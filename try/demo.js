f40b6b64af860fe642f363628f462ed6781cc757
zj347041359 <33504754+zj347041359@users.noreply.github.com>
2019-06-24 13:23:07 +0800

    add

---
+++ try/demo.js
@@ -0,0 +1,4 @@
+console.log('德玛西亚');
+function test(){
+    console.log('a');
+}
--- zj.js
+++ zj.js
@@ -37,10 +37,65 @@ Array.prototype.unique = function () {
         arr = [],
         len = this.length
     for(var i = 0; i < len; i++){
-        if(!obj[this.[i]]){
+        if(!obj[this[i]]){
             obj[this[i]] = 'abc';
             arr.push(this[i]);
         }
     }
     return arr;
+}
+
+//返回滚动条位置，兼容性方法
+function getScrollOffset(){
+    if(window.pageXOffset){
+        return {
+            x : window.pageXOffset,
+            y : window.pageYOffset
+        }
+    }else{
+        return {
+            x : document.body.scrollLeft + document.documentElement.scrollLeft,
+            y : document.body.scrollTop + document.documentElement.scrollTop
+        }
+    }
+}
+//返回窗口尺寸，兼容性方法
+function getViewportOffset(){
+    if(window.innerHeight){
+        return {
+            w : window.innerWidth,
+            h : window.innerHeight
+        }
+    }else{
+        if(document.compatMode === 'BackCompat'){
+            return {
+                w : document.body.clientWidth,
+                h : document.body.clientHeight
+            }
+        }else{
+            return {
+                w : document.documentElement.clientWidth,
+                h : document.documentElement.clientHeight
+            }
+        }
+    }
+}
+//js加载完成之后执行，兼容性写法
+function loadScript(url, callback) {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    document.head.appendChild(script);
+    if(script.readyState){
+        script.onreadystatechange = function(){
+            if(script.readyState == 'complete' || script.readyState == 'loaded'){
+                callback();
+            }
+        }
+    }else{
+        script.onload = function(){//非IE
+            callback();
+        }
+    }
+    script.src = 'url';
+    document.head.appendChild(script);
 }
