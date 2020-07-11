cf5a5c4708558618e22f02ed74fca2149e08cf81
ZJ <33504754+zj347041359@users.noreply.github.com>
2020-07-11 12:10:00 +0800

    add some

---
+++ wechat-try/pages/prompt/recruit.js
@@ -0,0 +1,17 @@
+//hot.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+  },
+  onLoad: function () {
+
+  },
+  onShow:function(){
+    // if(!app.globalData.rawData){
+    //   wx.switchTab({url:"/pages/index/index"})
+    // }
+  },
+
+})
